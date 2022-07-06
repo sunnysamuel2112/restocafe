064d2a9138e9e4906709fe6b1b1c588484bb97bd
Am096 <amithjamadagni540@gmail.com>
2022-07-06 19:27:44 +0530

    Created Order Model Created Order Adapter Completed Until 1:05:40

---
+++ app/src/main/java/com/example/restocafe/Adapters/OrdersAdapter.java
@@ -0,0 +1,46 @@
+package com.example.restocafe.Adapters;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.restocafe.R;
+
+public class OrdersAdapter extends RecyclerView.Adapter<OrdersAdapter.viewHolder>{
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+        ImageView orderImage;
+        TextView soldItemName, orderNumber, price;
+
+
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            orderImage = itemView.findViewById(R.id.orderImage);
+            soldItemName = itemView.findViewById(R.id.orderItemName);
+            orderNumber = itemView.findViewById(R.id.orderNumber);
+            price = itemView.findViewById(R.id.textViewPrice);
+        }
+    }
+
+    ;
+}
